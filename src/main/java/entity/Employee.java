c2a60b541137cc70ef7c7b44c6a9e7949e90739a
Boualem Ait Ali Slimane <b.aitalislimane@gmail.com>
2021-07-02 14:39:58 -0400

    Initial commit

---
+++ src/main/java/entity/Employee.java
@@ -0,0 +1,47 @@
+package entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+
+@Entity
+public class Employee {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+    @Column(name = "FNAME")
+    private String firstName;
+    @Column(name = "LNAME")
+    private String lastName;
+
+    public Employee() {
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    @Id
+    public Long getId() {
+        return id;
+    }
+
+    public String getfFirstName() {
+        return firstName;
+    }
+
+    public void setfFirstName(String fName) {
+        this.firstName = fName;
+    }
+
+    public String getlLastName() {
+        return lastName;
+    }
+
+    public void setlLastName(String lName) {
+        this.lastName = lName;
+    }
+
+}
